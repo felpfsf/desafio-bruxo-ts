d63870ebc2bdca035afe5ec5d0f882cd97c412fe
Felipe <feferreira.sf@gmail.com>
2023-03-04 17:33:18 -0300

    feat: added home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from "./style";
+
+export const Home = () => {
+  return (
+    <Container>
+      <h1>Olá eu sou a Home page</h1>
+    </Container>
+  );
+};
---
+++ src/pages/Home/style.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.main`
+  max-width: 1440px;
+  width: 100%;
+  margin: auto;
+  padding-bottom: 8px;
+  display: flex;
+  flex-grow: 1;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
