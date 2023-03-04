8a6cf8effad4b7b9f86b114b579e760bc52a9443
Felipe <feferreira.sf@gmail.com>
2023-03-04 17:25:12 -0300

    feat: container layout import navbar, home, footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import { ThemeProvider } from "styled-components";
+import { Footer } from "./components/Footer";
+import { Navbar } from "./components/Navbar";
+import { Home } from "./pages/Home";
 import { GlobalStyles } from "./styles/global";
 import { theme } from "./styles/theme";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <div>Desafio Bruxo</div>
+      <Container>
+        <Navbar />
+        <Home />
+        <Footer />
+      </Container>
     </ThemeProvider>
   );
 }
