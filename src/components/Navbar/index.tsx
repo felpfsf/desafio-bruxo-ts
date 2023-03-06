47395acac9645b5624336c53247d143363e73923
Felipe <feferreira.sf@gmail.com>
2023-03-06 20:53:11 -0300

    feat: working on custom select

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,8 +1,26 @@
-import {NavbarContainer} from './style'
+import { useState } from "react";
+import { ANCESTRY_OPTIONS, HOUSE_OPTIONS } from "../../utils";
+import { CustomSelect } from "../CustomSelect";
+import { NavbarContainer } from "./style";
 export const Navbar = () => {
+  const [currentHouseValue, setCurrentHouseValue] = useState("");
   return (
     <NavbarContainer>
       <h1>Desafio Bruxo</h1>
+      <nav>
+        <CustomSelect
+          options={HOUSE_OPTIONS}
+          currentValue={currentHouseValue}
+          id='house-select'
+          handleChangeValue=''
+        />
+        <CustomSelect
+          options={ANCESTRY_OPTIONS}
+          currentValue={currentHouseValue}
+          id='ancestry-select'
+          handleChangeValue=''
+        />
+      </nav>
     </NavbarContainer>
   );
 };
