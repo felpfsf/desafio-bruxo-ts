ca52cd37b1e4c874b4ef84ec456d25e1616cd524
Felipe <feferreira.sf@gmail.com>
2023-03-06 20:52:28 -0300

    feat: import custom select to navbar

---
+++ src/utils/constants.ts
@@ -0,0 +1,22 @@
+interface OptionsProps {
+  id: number;
+  value: string;
+  label: string;
+}
+
+export const HOUSE_OPTIONS: OptionsProps[] = [
+  { id: 1, value: "all", label: "Todas as casas" },
+  { id: 2, value: "Gryffindor", label: "Gryffindor" },
+  { id: 3, value: "Slytherin", label: "Slytherin" },
+  { id: 4, value: "Ravenclaw", label: "Ravenclaw" },
+  { id: 5, value: "Hufflepuff", label: "Hufflepuff" },
+];
+
+export const ANCESTRY_OPTIONS: OptionsProps[] = [
+  { id: 1, value: "all", label: "Todos" },
+  { id: 2, value: "pure-blood", label: "Puro-Sangue" },
+  { id: 3, value: "half-blood", label: "Mestiço" },
+  { id: 4, value: "muggleborn", label: "Nascido Trouxa" },
+  { id: 5, value: "squib", label: "Cepa torta" },
+  { id: 5, value: "muggle", label: "Trouxa" },
+];
