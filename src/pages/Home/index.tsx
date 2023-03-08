6742378d52253af735fe4e40d39fd64755eb40a1
Felipe <feferreira.sf@gmail.com>
2023-03-08 14:09:41 -0300

    feat: imported fetchData hook

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,9 +2,13 @@ import { CharacterCard } from "../../components/CharacterCard";
 import { CustomSelect } from "../../components/CustomSelect";
 import { Loading } from "../../components/Loading";
 import { NotFound } from "../../components/NotFound";
+import { useFetchData } from "../../hooks/useFetch";
+import { API_URL } from "../../utils/env";
 import { CharacterList, Container } from "./style";
 
 export const Home = () => {
+  const {data} = useFetchData(API_URL)
+  console.log(data)
   return (
     <Container>
       <h1>Olá eu sou a Home page</h1>
