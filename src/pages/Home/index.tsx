2503e929cf37a581f8f5d39997768a59b5865978
Felipe <feferreira.sf@gmail.com>
2023-03-08 17:09:45 -0300

    feat: rendering character cards and data

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,25 +1,55 @@
+import { useEffect, useState } from "react";
 import { CharacterCard } from "../../components/CharacterCard";
 import { CustomSelect } from "../../components/CustomSelect";
 import { Loading } from "../../components/Loading";
 import { NotFound } from "../../components/NotFound";
+import { useAppContext } from "../../hooks/useDataStore";
 import { useFetchData } from "../../hooks/useFetch";
+import { CharacterProps } from "../../types";
 import { API_URL } from "../../utils/env";
 import { CharacterList, Container } from "./style";
 
 export const Home = () => {
-  const {data} = useFetchData(API_URL)
-  console.log(data)
+  const { filters } = useAppContext();
+  const {
+    data: characters,
+    error,
+    status,
+  } = useFetchData<CharacterProps[]>(API_URL);
+  const [filteredCharacters, setFilteredCharacters] = useState<
+    CharacterProps[]
+  >([]);
+  const [charactersToShow, setCharactersToShow] = useState<number>(10);
+
+  useEffect(() => {
+    if (characters) {
+      const filtered = characters.filter((character) => {
+        if (filters.house && character.house !== filters.house) {
+          return false;
+        }
+        if (filters.ancestry && character.ancestry !== filters.ancestry) {
+          return false;
+        }
+        if (
+          filters.name &&
+          !character.name.toLowerCase().includes(filters.name)
+        ) {
+          return false;
+        }
+        return true;
+      });
+      setFilteredCharacters(filtered);
+    }
+  }, [filters, characters]);
+
   return (
     <Container>
-      <h1>Olá eu sou a Home page</h1>
       <CharacterList>
-        {/* <CharacterCard />
-        <CharacterCard />
-        <CharacterCard />
-        <CharacterCard /> */}
-        {/* <NotFound /> */}
-        {/* <Loading /> */}
-        {/* <CustomSelect /> */}
+        {status === "loading" && <Loading />}
+        {filteredCharacters.length &&
+          filteredCharacters
+            .slice(0, charactersToShow)
+            .map((character) => <CharacterCard key={character.id} />)}
       </CharacterList>
     </Container>
   );
