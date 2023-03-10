627d5ed01ccab902a10be6894824210eead8b0de
Felipe <feferreira.sf@gmail.com>
2023-03-10 18:06:55 -0300

    style: width style

--- src/components/NotFound/style.ts
+++ src/components/NotFound/style.ts
@@ -6,6 +6,17 @@ export const Box = styled.div`
   flex-direction: column-reverse;
   align-items: center;
   gap: 1rem;
+  @media ${({ theme }) => theme.devices.tablet} {
+    width: 50%;
+    position: relative;
+    & h1 {
+      position: absolute;
+      top: 45%;
+      font-family: ${({ theme }) => theme.title};
+      font-size: ${({ theme }) => theme.large};
+      font-weight: 700;
+    }
+  }
 `;
 
 export const ImageBox = styled.div`
@@ -14,5 +25,8 @@ export const ImageBox = styled.div`
     width: 100%;
     height: 100%;
     object-fit: cover;
+  }
+  @media ${({ theme }) => theme.devices.tablet} {
+    width: 25%;
   }
 `;
